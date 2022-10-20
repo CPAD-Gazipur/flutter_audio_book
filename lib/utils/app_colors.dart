e2fc92b7e47b5f453bc6c57309025f6b08e3d1a0
alamin-karno <amin15-1951@diu.edu.bd>
2022-10-20 09:58:21 +0600

    Audio Book: App Colors File Added.

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFFFAFAFC);
+  static const Color silverBackgroundColor = Color(0xFFFAFAFC);
+  static const Color menu1Color = Color(0xFFF9D263);
+  static const Color menu2Color = Color(0xFFF2603D);
+  static const Color menu3Color = Color(0xFF04ABE6);
+  static const Color tabBarViewColor = Color(0xFFFDFDFD);
+  static const Color starColor = Color(0xFFFDC746);
+  static const Color subTitleColor = Color(0xFFC5C4CA);
+  static const Color loveColor = Color(0xFF00ACE6);
+  static const Color audioBlueBackgroundColor = Color(0xFFEEEFFA);
+  static const Color audioGreyBackgroundColor = Color(0xFFF2F2F2);
+}
