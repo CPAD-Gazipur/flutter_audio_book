9d6d2ec2ff562e940dc7313c04988e69d604a74b
alamin-karno <amin15-1951@diu.edu.bd>
2022-10-20 11:24:35 +0600

    Audio Book: Load Popular Books From JSON File and Add on ViewPager Builder.

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import '../utils/utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,13 +12,117 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List popularBooks = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    readJSONFiles();
+  }
+
+  readJSONFiles() async {
+    await DefaultAssetBundle.of(context)
+        .loadString('assets/json/popular_books.json')
+        .then((popularBookJSON) {
+      setState(() {
+        popularBooks = json.decode(popularBookJSON);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Audio Book'),
-        centerTitle: true,
-        elevation: 0,
+    return Container(
+      color: AppColors.backgroundColor,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: AppColors.backgroundColor,
+          body: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  left: 15,
+                  right: 15,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const ImageIcon(
+                      AssetImage('assets/images/menu.png'),
+                      size: 18,
+                      color: Colors.black,
+                    ),
+                    Row(
+                      children: const [
+                        Icon(Icons.search),
+                        SizedBox(width: 15),
+                        Icon(Icons.notifications),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                margin: const EdgeInsets.only(
+                  left: 15,
+                ),
+                child: Row(
+                  children: const [
+                    Text(
+                      'Popular Books',
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15),
+              SizedBox(
+                height: 160,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      left: -20,
+                      right: -25,
+                      child: SizedBox(
+                        height: 160,
+                        child: PageView.builder(
+                          controller: PageController(viewportFraction: 0.8),
+                          itemCount:
+                              popularBooks.isEmpty ? 0 : popularBooks.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              height: 160,
+                              width: MediaQuery.of(context).size.width,
+                              margin: const EdgeInsets.only(right: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    popularBooks[index]['image'],
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                                border: Border.all(
+                                  color: Colors.grey,
+                                  width: 1,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
